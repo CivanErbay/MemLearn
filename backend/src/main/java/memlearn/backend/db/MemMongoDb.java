1051161903ccb4dd3275cf074038d5be8bf6560a
Civan Erbay <Civan.Erbay@web.de>
2020-06-26 16:53:55 +0200

    Remove nodemodules

--- backend/src/main/java/memlearn/backend/db/MemMongoDb.java
+++ backend/src/main/java/memlearn/backend/db/MemMongoDb.java
@@ -2,6 +2,8 @@ package memlearn.backend.db;
 
 import memlearn.backend.model.MemCard;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemMongoDb extends PagingAndSortingRepository<MemCard, String> {
 }
